72a120e6cb47dc866a6bc1e615f84fcbee721356
Saimon Moore <saimon.moore@xing.com>
2020-04-04 21:03:08 +0200

    Wip - Clickable letters
    TODO: Make clickable letter play audio on click and show image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import flors from './flors.jpg';
+import nazhseis from './nazhseisjoanne.m4a';
 import './App.css';
+
+const AudioPlay = ({src, shouldPlay}) => {
+  const audioPlayer = new Audio(src);
+
+  if (shouldPlay) {
+    audioPlayer.play()
+  } else {
+    audioPlayer.pause();
+  }
+
+  return ("");
+};
+
+const ClickableLetter = (props) => {
+  return (
+    <span>
+    <strong style={{ color: 'seagreen', fontSize: '90px', cursor: 'pointer', marginRight: '20px'}} onClick={props.action}>{props.letter}</strong>
+    {props.children}
+    </span>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{ color: 'darksalmon'}}>Happy Birthday</h1>
+        <img src={flors}  width={300} height={200}/>
+        <div>
+      <ClickableLetter letter={"J"} action={() => { console.log("Clicked J...");}}>
+        <AudioPlay src={nazhseis} shouldPlay={false} />
+      </ClickableLetter>
+
+      <ClickableLetter letter={"O"} />
+      <ClickableLetter letter={"A"} />
+      <ClickableLetter letter={"N"} />
+      <ClickableLetter letter={"N"} />
+      <ClickableLetter letter={"E"} />
+        </div>
       </header>
     </div>
   );
