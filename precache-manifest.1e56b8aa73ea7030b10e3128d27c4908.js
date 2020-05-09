778156e052286cff989f68e038b7e6dd52c2a8bb
Saimon Moore <saimon.moore@xing.com>
2020-05-09 13:23:32 +0200

    Updates

---
+++ precache-manifest.1e56b8aa73ea7030b10e3128d27c4908.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9028d05c1c307450118ee160b7e24476",
+    "url": "/joannes-birthday-2020/index.html"
+  },
+  {
+    "revision": "b8076be7a7505423014c",
+    "url": "/joannes-birthday-2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b92c2abbdf2dddce4e0",
+    "url": "/joannes-birthday-2020/static/js/2.cdcade5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joannes-birthday-2020/static/js/2.cdcade5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8076be7a7505423014c",
+    "url": "/joannes-birthday-2020/static/js/main.7ab84d1c.chunk.js"
+  },
+  {
+    "revision": "d5ec7d22b9f681b2bc61",
+    "url": "/joannes-birthday-2020/static/js/runtime-main.3595984c.js"
+  },
+  {
+    "revision": "344b3f86ef7c416b4c253f7703088331",
+    "url": "/joannes-birthday-2020/static/media/elia.344b3f86.m4a"
+  },
+  {
+    "revision": "79e8dcbb4077ee4f9ee6c7da6266c74d",
+    "url": "/joannes-birthday-2020/static/media/elia.79e8dcbb.jpg"
+  },
+  {
+    "revision": "64ca6b61b3549b1c0b1b6d23f9264bca",
+    "url": "/joannes-birthday-2020/static/media/flors.64ca6b61.jpg"
+  },
+  {
+    "revision": "7edb06aa8a4f47666a386d956cd10027",
+    "url": "/joannes-birthday-2020/static/media/mario.7edb06aa.ogg"
+  },
+  {
+    "revision": "e0792f917a86babf3359dd8693746984",
+    "url": "/joannes-birthday-2020/static/media/mario.e0792f91.jpg"
+  },
+  {
+    "revision": "07c90c782ae6dac095275014f8bb5dda",
+    "url": "/joannes-birthday-2020/static/media/nazhseisjoanne.07c90c78.m4a"
+  },
+  {
+    "revision": "77012731d1837a288b23aaf1e86bbdb5",
+    "url": "/joannes-birthday-2020/static/media/pol.77012731.m4a"
+  },
+  {
+    "revision": "bc29f3cc50540b35b8c36f5dfc8c3736",
+    "url": "/joannes-birthday-2020/static/media/pol.bc29f3cc.jpg"
+  },
+  {
+    "revision": "506b568c905ee5a48c314510bde36923",
+    "url": "/joannes-birthday-2020/static/media/saimon-eulalia.506b568c.jpg"
+  }
+]);
